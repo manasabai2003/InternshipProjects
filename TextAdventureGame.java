44cef3d5299437529b576492c493bf1eb4274c16
manasabai2003 <151177248+manasabai2003@users.noreply.github.com>
2023-12-14 19:01:26 +0530

    TextAdventureGame.java

--- TextAdventureGame.java
+++ TextAdventureGame.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
-
 public class TextAdventureGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to the Text Adventure Game!");
         System.out.println("You find yourself in a dark cave. You see two paths ahead.");
 
         // Decision point 1
         System.out.println("1. Take the left path");
         System.out.println("2. Take the right path");
-
         int decision1 = getUserChoice(scanner, 2);
-
         if (decision1 == 1) {
             // Outcome for left path
             System.out.println("You find a treasure chest! Congratulations, you're rich!");
-        } else {
+        }
+        else {
             // Outcome for right path
             System.out.println("Uh-oh! A monster appears and chases you. You barely escape.");
         }
@@ -25,17 +22,15 @@ public class TextAdventureGame {
         System.out.println("You continue your journey and reach a crossroads.");
         System.out.println("1. Go straight ahead");
         System.out.println("2. Turn back");
-
         int decision2 = getUserChoice(scanner, 2);
-
         if (decision2 == 1) {
             // Outcome for going straight
             System.out.println("You discover a hidden passage. It leads you to safety.");
-        } else {
+        } 
+        else {
             // Outcome for turning back
             System.out.println("You encounter a dead-end. It's a trap! Game over.");
         }
-
         System.out.println("Thanks for playing the Text Adventure Game!");
     }
 
@@ -43,22 +38,22 @@ public class TextAdventureGame {
     private static int getUserChoice(Scanner scanner, int maxChoice) {
         int userChoice = 0;
         boolean validInput = false;
-
         while (!validInput) {
             System.out.print("Enter your choice: ");
             if (scanner.hasNextInt()) {
                 userChoice = scanner.nextInt();
                 if (userChoice >= 1 && userChoice <= maxChoice) {
                     validInput = true;
-                } else {
+                } 
+                else {
                     System.out.println("Invalid input. Please enter a number between 1 and " + maxChoice + ".");
                 }
-            } else {
+            } 
+            else {
                 System.out.println("Invalid input. Please enter a valid number.");
                 scanner.next();
             }
         }
-
         return userChoice;
     }
 }
